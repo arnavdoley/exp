954c76b7306469c0f338e4ec7e82a34bda813796
arnavdoley <arnavdoley.ad@gmail.com>
2024-06-27 11:54:22 +0530

    name

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By{"Arnav "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
